e0e8e0797814efdd10dff062264943fd0aec82c2
villainouskirby <69393156+villainouskirby@users.noreply.github.com>
2025-03-21 23:45:31 +0900

    UI Handler 추가

---
+++ Assets/Scripts/UI/UIPopUpHandler.cs
@@ -0,0 +1,113 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UIPopUpHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject _inventory;
+    [SerializeField] private GameObject _shop;
+    [SerializeField] private GameObject _storage;
+    [SerializeField] private GameObject _quest;
+    [SerializeField] private GameObject _smith;
+
+    private Inventory _inventoryScript;
+    private Shop _shopScript;
+    private Storage _storageScript;
+
+    private List<GameObject> _openUIs = new List<GameObject>();
+
+    private static UIPopUpHandler _instance;
+    public static UIPopUpHandler Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.Log("인스턴스가 존재하지 않음");
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    private void Start()
+    {
+        _inventory.SetActive(true);
+        _shop.SetActive(true);
+        _storage.SetActive(true);
+        _quest.SetActive(true);
+        _smith.SetActive(true);
+
+        _inventory.SetActive(false);
+        _shop.SetActive(false);
+        _storage.SetActive(false);
+        _quest.SetActive(false);
+        _smith.SetActive(false);
+
+        _inventoryScript = _inventory.GetComponent<Inventory>();
+        _shopScript = _shop.GetComponent<Shop>();
+        _storageScript = _storage.GetComponent<Storage>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            GameObject clickedObject = EventSystem.current.currentSelectedGameObject;
+            if (clickedObject == null || !_openUIs.Contains(clickedObject)) return;
+
+            _openUIs.Remove(clickedObject);
+            _openUIs.Add(clickedObject);
+        }
+
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            if (_openUIs.Count > 0)
+            {
+                GameObject lastUI = _openUIs[_openUIs.Count - 1];
+
+                if (lastUI == _storage) 
+
+                lastUI.SetActive(false);
+                _openUIs.Remove(lastUI);
+            }
+        }
+    }
+
+    private void OpenUI(GameObject ui)
+    {
+        ui.SetActive(true);
+        if (!_openUIs.Contains(ui)) _openUIs.Add(ui);
+    }
+
+    /// <summary> 인벤토리 열기 </summary>
+    public void OpenInventory() // TODO => 매번 호출할때마다 초기화시키는건 비효율적인데 뭔가 개선방안이 필요할듯
+    {
+        _inventoryScript.InitInventory();
+        OpenUI(_inventory);
+    }
+
+    /// <summary> 상점 열기 </summary>
+    public void OpenShop()
+    {
+        _shopScript.InitInvenToShop();
+        OpenUI(_shop);
+    }
+
+    /// <summary> 창고 열기 </summary>
+    public void OpenStorage()
+    {
+        _storageScript.InitInventory();
+        OpenUI(_storage);
+    }
+
+    /// <summary> 퀘스트 게시판 열기 </summary>
+    public void OpenQuest() => OpenUI(_quest);
+
+    /// <summary> 대장장이 UI 열기</summary>
+    public void OpenSmith() => OpenUI(_smith);
+}
