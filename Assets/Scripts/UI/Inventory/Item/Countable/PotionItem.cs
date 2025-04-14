85e671d87e5e3949a26aba624d686738f6c043f4
Doumoman <60395262+Doumoman@users.noreply.github.com>
2025-04-14 16:10:33 +0900

    플레이어 HP, 스테미나

--- Assets/Scripts/UI/Inventory/Item/Countable/PotionItem.cs
+++ Assets/Scripts/UI/Inventory/Item/Countable/PotionItem.cs
@@ -15,7 +15,15 @@ public class PotionItem : CountableItem, IUsableItem
     {
         if (Amount > 0)
         {
-            BuffManager.instance.CreateBuff(Data.ID, percentage: 0.4f, duration: 10f, Data.IconSprite);
+            if (Data.ID <= 10) // 1~10은 버프 포션
+            {
+                BuffManager.instance.CreateBuff(Data.ID, percentage: 0.4f, duration: 10f, Data.IconSprite);
+                //TODO : 스크립터블 오브젝트에서 포션 데이터 얻는 로직 고민해보기
+            }
+            else if (Data.ID <= 20) // 11~20은 HP 회복 포션
+            {
+                PlayerData.instance.HPValueChange(10f); //TODO : 스크립터블 오브젝트에서 힐량 얻기
+            }
             Amount--;
             return true;
         }
