00a33b5045a7179a02db113cb2ff18c1adee3627
catsnakedog <103244627+catsnakedog@users.noreply.github.com>
2025-10-25 00:36:16 +0900

    그림자 버그 해결

--- Assets/TileMapCustom/PlayerFarming/PlayerFarming.cs
+++ Assets/TileMapCustom/PlayerFarming/PlayerFarming.cs
@@ -55,6 +55,8 @@ public class PlayerFarming : MonoBehaviour
 
     private void Start()
     {
+        FarmingKey = KeyCode.F;
+        DropItemKey = KeyCode.U;
         _instance = this;
         _rangedResourceNodeObj = new();
         TargetResourceNode = null;
